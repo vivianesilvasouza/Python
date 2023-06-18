f4c74f4bd8821f56c424005e5f147f89e0162def
Viviane da silva souza <vivianedasilvasouza2020@gmail.com>
2023-06-18 19:05:27 -0300

    feat: estudos

--- revisao/aula1.py
+++ revisao/aula1.py
@@ -1,7 +1,30 @@
-nome = 'viviane'
-altura = 1.58
-peso = 72
-imc = peso / altura * 2
+n = 5
+execution_time = [3, 4, 1, 7, 6]
+x = 4
+y = 2
 
 
-print('seu nome é ' , nome ,'sua altura é ' ,altura , ' e seu  índice de imc é ' , imc)
+def can_execute_all_tasks(n, l, x, y):
+    execution_time = l.copy()
+    num_operations = 0
+
+    while len(execution_time) > 0:
+        # escolha a tarefa principal (a que tem maior tempo de execução)
+        main_task_index = execution_time.index(max(execution_time))
+        execution_time[main_task_index] -= x
+
+        # reduz o tempo de execução das outras tarefas
+        for i in range(len(execution_time)):
+            if i != main_task_index and execution_time[i] > 0:
+                execution_time[i] -= y
+
+        # remove as tarefas concluídas
+        execution_time = [t for t in execution_time if t > 0]
+        num_operations += 1
+
+    return num_operations
+
+    
+    num_operations = can_execute_all_tasks(n, execution_time, x, y)
+
+print(num_operations)
---
+++ revisao/aulaDois.py
@@ -0,0 +1,15 @@
+""" entrada = input('voce quer "entrar" ou "sair"? ')
+
+if entrada == "entrar":
+    print("você entrou no sistemas")
+elif entrada == "sair":
+    print("voce saiu do programa")
+else:
+    print("você não digitou nem entrar e nem sair.") """
+
+pergunta = input("você gosta do harry potter?")
+
+if pergunta == "sim":
+    print("bem vindo a Hogwarts!")
+else:
+    print("Que pena, você é um trouxa! ")
